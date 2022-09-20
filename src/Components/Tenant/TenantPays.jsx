f05f66b7397cf4a2093d509dbb978031e0faf663
Dimanok1989 <kolgaev@gmail.com>
2022-09-20 13:14:09 +0300

    Добавление платежей

--- src/Components/Tenant/TenantPays.jsx
+++ src/Components/Tenant/TenantPays.jsx
@@ -82,50 +82,6 @@ const NextPayRow = props => {
 
     }, [addReady]);
 
-    React.useEffect(() => {
-
-        if (save) {
-            axios.post('/incomes/save', formdata)
-                .then(({ data }) => {
-                    setAddReady(false);
-                    setRow(p => {
-
-                        let source = { ...p, ...data.source },
-                            pays = [...(p.pays || [])],
-                            next_pays = [...(p.next_pays || [])];
-
-                        source.pays = [];
-                        source.next_pays = [];
-
-                        next_pays.forEach(r => {
-                            if (row.date != r.date)
-                                source.next_pays.push(r);
-                        });
-
-                        let add = true;
-
-                        pays.forEach(r => {
-                            if (r.month === data.row.month) {
-                                add = false;
-                                r.rows.unshift(data.pay);
-                            }
-
-                            source.pays.push(r);
-                        });
-
-                        add && source.pays.unshift({
-                            month: data.row.month,
-                            rows: [data.pay],
-                        });
-
-                        return source;
-                    });
-                })
-                .catch(e => setSaveError(axios.post(e)))
-                .then(() => setSave(false));
-        }
-
-    }, [save]);
 
     return <div className="position-relative pay-row-tenant px-3" style={{ minHeight: 31 }}>
 
@@ -135,54 +91,27 @@ const NextPayRow = props => {
 
         <div className="flex-grow-1">{row.title}</div>
 
-        {addReady && <div style={{
-            position: "absolute",
-            right: "-0.25rem",
-            bottom: -1,
-            top: 0,
-            left: 0,
-            background: "#ffffffc4",
-            textAlign: "right",
-        }}>
-            <Button
-                icon="cancel"
-                size="mini"
-                basic
-                className="mx-1"
-                onClick={() => setAddReady(false)}
-                disabled={save}
+        <span>
+            <Icon
+                name="plus"
+                fitted
+                link
+                title="Добавить платеж"
+                onClick={() => setAddReady(true)}
             />
-            <Input
-                size="mini"
-                type="date"
-                className="me-1"
-                style={{ maxHeight: 30.25 }}
-                value={formdata.date || ""}
-                onChange={(e, { value }) => setFormdata(p => ({ ...p, date: value }))}
-                disabled={save}
-            />
-            <Input
-                size="mini"
-                placeholder="Введите сумму"
-                type="number"
-                step="0.01"
-                value={formdata.sum || ""}
-                onChange={(e, { value }) => setFormdata(p => ({ ...p, sum: value }))}
-                className="me-1"
-                disabled={save}
-            />
-            <Button
-                icon="save"
-                size="mini"
-                basic
-                color={saveError ? "red" : "green"}
-                onClick={() => setSave(true)}
-                loading={save}
-                disabled={save}
-            />
-        </div>}
+        </span>
 
-        {!addReady && <span><Icon name="plus" fitted link title="Добавить платеж" onClick={() => setAddReady(true)} /></span>}
+        <AddPayForm
+            show={addReady}
+            close={() => setAddReady(false)}
+            setRow={setRow}
+            date={moment().format("YYYY-MM-DD")}
+            sum={row.price}
+            purpose_pay={row.type}
+            income_part_id={income_part_id}
+            income_source_id={income_source_id}
+            nextPayRow={row}
+        />
 
     </div>
 }
@@ -191,8 +120,9 @@ const PayRowColumn = props => {
 
     const { month, row, setRow, source } = props;
     const [load, setLoad] = React.useState(false);
+    const [addPay, setAddPay] = React.useState(false);
 
-    const className = ["pay-row-tenant"];
+    const className = ["pay-row-tenant position-relative"];
     !Boolean(row.purpose_every_month) && className.push("pay-row-type-one");
 
     let pay_color = Boolean(row.sum) ? "green" : (Boolean(row.hide_overdue) ? "red" : null)
@@ -322,21 +252,190 @@ const PayRowColumn = props => {
 
             </>}
 
-            {!Boolean(row.sum) && <span>
-                <Icon
-                    name={!Boolean(row.hide_overdue) ? "eye slash" : "eye"}
-                    link={load === false}
-                    disabled={load !== false}
-                    title="Не учитывать как просроченное"
-                    onClick={() => setHideOverdue({
-                        month: month,
-                        purpose: row?.purpose?.id,
-                        source_id: source?.id,
-                    })}
-                />
-            </span>}
+            {!Boolean(row.sum) && <>
+
+                <span>
+                    <Icon
+                        name="plus"
+                        link
+                        title="Добавить платеж"
+                        onClick={() => setAddPay(true)}
+                    />
+                </span>
+
+                <span>
+                    <Icon
+                        name={!Boolean(row.hide_overdue) ? "eye slash" : "eye"}
+                        link={load === false}
+                        disabled={load !== false}
+                        title="Не учитывать как просроченное"
+                        onClick={() => setHideOverdue({
+                            month: month,
+                            purpose: row?.purpose?.id,
+                            source_id: source?.id,
+                        })}
+                    />
+                </span>
+
+            </>}
         </div>
 
+        {!Boolean(row.sum) && <AddPayForm
+            show={addPay}
+            close={() => setAddPay(false)}
+            setRow={setRow}
+            date={moment(row.date).format("YYYY-MM-DD")}
+            sum={Number(row?.source?.price) * Number(row?.source?.space)}
+            purpose_pay={row.purpose_pay}
+            income_part_id={row?.source?.part_id}
+            income_source_id={row.income_source_id}
+            lostPayRow={row}
+        />}
+
+    </div>
+
+}
+
+const AddPayForm = props => {
+
+    const { show, close, setRow } = props;
+    const { date, sum, purpose_pay, income_part_id, income_source_id } = props;
+    const { nextPayRow, lostPayRow } = props;
+
+    const [formdata, setFormdata] = React.useState({});
+    const [save, setSave] = React.useState(false);
+    const [saveError, setSaveError] = React.useState(false);
+
+    React.useEffect(() => {
+
+        if (show) {
+            setFormdata({ date, sum, purpose_pay, income_part_id, income_source_id });
+            console.log(props);
+        }
+
+    }, [show]);
+
+    React.useEffect(() => {
+
+        if (save) {
+            axios.post('/incomes/save', formdata)
+                .then(({ data }) => {
+
+                    typeof close == "function" && close();
+
+                    typeof setRow == "function" && setRow(p => {
+
+                        let source = { ...p, ...data.source },
+                            pays = [...(p.pays || [])],
+                            next_pays = [...(p.next_pays || [])];
+
+                        source.pays = [];
+
+                        if (nextPayRow) {
+
+                            source.next_pays = [];
+
+                            next_pays.forEach(r => {
+                                if (nextPayRow.date != r.date)
+                                    source.next_pays.push(r);
+                            });
+                        } else {
+                            source.next_pays = next_pays;
+                        }
+
+                        let add = true,
+                            month = null;
+
+                        if (lostPayRow)
+                            month = moment(lostPayRow.date).format("YYYY-MM");
+
+                        pays.forEach(r => {
+
+                            let monthRows = [];
+
+                            if (r.month === data.row.month) {
+                                add = false;
+                                monthRows.push(data.pay);
+                            }
+
+                            r.rows.forEach(rr => {
+
+                                if (
+                                    month
+                                    && !Boolean(rr.sum)
+                                    && month === data.pay.month
+                                    && rr.purpose_pay === data.pay.purpose_pay
+                                ) return;
+
+                                monthRows.push(rr);
+                            });
+
+                            source.pays.push({ ...r, rows: monthRows });
+                        });
+
+                        add && source.pays.unshift({
+                            month: data.row.month,
+                            rows: [data.pay],
+                        });
+
+                        return source;
+                    });
+                })
+                .catch(e => setSaveError(axios.post(e)))
+                .then(() => setSave(false));
+        }
+
+    }, [save]);
+
+    if (!show)
+        return null;
+
+    return <div
+        style={{
+            position: "absolute",
+            right: 0,
+            bottom: nextPayRow ? 1 : 0,
+            top: nextPayRow ? 1 : 0,
+            left: 0,
+            background: "#ffffffe6",
+        }}
+        className="d-flex justify-content-end"
+    >
+        <Input
+            size="mini"
+            type="date"
+            className="me-1"
+            style={{ maxHeight: "100%" }}
+            value={formdata.date || ""}
+            onChange={(e, { value }) => setFormdata(p => ({ ...p, date: value }))}
+            disabled={save}
+        />
+        <Input
+            size="mini"
+            placeholder="Введите сумму"
+            type="number"
+            step="0.01"
+            value={formdata.sum || ""}
+            onChange={(e, { value }) => setFormdata(p => ({ ...p, sum: value }))}
+            className="me-1"
+            disabled={save}
+        />
+        <Button
+            icon="save"
+            size="mini"
+            color={saveError ? "red" : "green"}
+            onClick={() => setSave(true)}
+            loading={save}
+            disabled={save}
+            className="me-1"
+        />
+        <Button
+            icon="cancel"
+            size="mini"
+            className="me-0"
+            onClick={() => close()}
+            disabled={save}
+        />
     </div>
 
 }
